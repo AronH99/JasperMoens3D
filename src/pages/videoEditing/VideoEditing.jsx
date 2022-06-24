e8b9b22b4bba149740f0bf3b2062597cd6d451da
AronH99 <aronheremans@gmail.com>
2022-06-24 14:08:20 +0200

    everything finished excpet for videoEditting

--- src/pages/videoEditing/VideoEditing.jsx
+++ src/pages/videoEditing/VideoEditing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const videoEditing = () => {
-  return <div>videoEditing</div>;
+  return <div className="videoContainer">videoEditing</div>;
 };
 
 export default videoEditing;
